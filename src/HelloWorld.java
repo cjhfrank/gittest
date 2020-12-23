ed31820801539d74736c751eaa8b3eee6b181294
cjhfrank <cjhfrank@gmail.com>
2020-12-23 23:48:26 +0800

    5th ver

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -4,5 +4,6 @@ public class HelloWorld {
 		System.out.println("2 st Hello World!");
 		System.out.println("3 st modify!");
 		System.out.println("4st modify!");
+		System.out.println("5st modify!");
 	}
 }
