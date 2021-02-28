155b513c60a012202f3a609bbc4f97845cf7b5c2
cjhfrank <cjhfrank@gmail.com>
2021-02-28 14:56:45 +0800

    input

--- src/HelloWorld.java
+++ src/HelloWorld.java
@@ -1,5 +1,6 @@
 public class HelloWorld {
 	public static void main(String[] args){
+		System.out.println("you input " + args[0]);
 		System.out.println("Hello World!");
 		System.out.println("2 st Hello World!");
 		System.out.println("3 st modify!");
